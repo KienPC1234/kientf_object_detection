873d301c96a8c87c2b4466896e317221e91a0f1b
Hà Trí Kiên (KCD Dev) <121302025+KienPC1234@users.noreply.github.com>
2025-02-18 07:12:02 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-#Modded By KienTF (KCD Dev) 
 import os
 from setuptools import find_packages, setup
 
 REQUIRED_PACKAGES = [
-    # Required for Apache Beam with Python 3
+    # Required for apache-beam with PY3
     'tensorflow>=2.0,<2.13',
     'pyyaml>=6.0.0',
     'avro-python3',
@@ -27,23 +26,23 @@ REQUIRED_PACKAGES = [
     'sacrebleu<=2.2.0'  # https://github.com/mjpost/sacrebleu/issues/209
 ]
 
-# Dynamically finding all modules in the current directory
-current_directory = os.path.dirname(os.path.abspath(__file__))
-module_directory = os.path.join(current_directory, 'object_detection')
 
 setup(
     name='kientf_object_detection',
     version='0.1',
     install_requires=REQUIRED_PACKAGES,
     include_package_data=True,
-    packages=find_packages(where=current_directory),  # Automatically find all packages
-    package_dir={  # Directory mapping for specific folders
+    packages=find_packages(where="."),  
+    package_dir={
+        'object_detection': 'object_detection',
         'datasets': os.path.join('slim', 'datasets'),
         'nets': os.path.join('slim', 'nets'),
         'preprocessing': os.path.join('slim', 'preprocessing'),
         'deployment': os.path.join('slim', 'deployment'),
-        'scripts': os.path.join('slim', 'scripts'),
+        'scripts': os.path.join('slim', 'scripts')
     },
     description='TensorFlow Object Detection Library With Modded By Kien',
+    long_description='This package is a modified version of TensorFlow\'s Object Detection Library, developed by KienTF for custom implementations and improvements.',
+    long_description_content_type='text/markdown',
     python_requires='>3.6',
 )
